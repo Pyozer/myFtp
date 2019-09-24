b948f76497a135bb3966a5ea61bbf43ac19467e1
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-09-24 09:22:30 +0200

    ad

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import Button from '@material-ui/core/Button';
+import { Input, TextField,Button,InputLabel, FormHelperText,FormControl } from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <p> Test </p>
-           <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <TextField
+        label="Adresse"
+        placeholder="[Adresse]:[port]"
+        className="text"
+        color="primary"
+        margin="normal"
+      /> 
+    <Button variant="contained" color="primary"> Send </Button>
+    <FormControl>
+  <InputLabel htmlFor="my-input">Email address</InputLabel>
+  <Input id="my-input" aria-describedby="my-helper-text" />
+  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+</FormControl>
       </header>
     </div>
   );
