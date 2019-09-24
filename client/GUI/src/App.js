a46141108c8467d54d718249b35e1feca270f208
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-09-24 17:26:29 +0200

    refactor Forms

--- client/GUI/src/App.js
+++ client/GUI/src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Input, TextField,Button,InputLabel, FormHelperText,FormControl } from '@material-ui/core';
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-    <TextField
-        label="Adresse"
-        placeholder="[Adresse]:[port]"
-        className="text"
-        color="primary"
-        margin="normal"
-      /> 
-    <Button variant="contained" color="primary"> Send </Button>
-    <FormControl>
-  <InputLabel htmlFor="my-input">Email address</InputLabel>
-  <Input id="my-input" aria-describedby="my-helper-text" />
-  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
-</FormControl>
+      <Form/>
       </header>
     </div>
   );
