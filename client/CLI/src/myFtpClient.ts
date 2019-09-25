62cd6764f5fbb36b7a9d99274710d0872ff4b2fc
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2019-09-25 14:29:32 +0200

    Improve code and fix build issue

--- client/CLI/src/myFtpClient.ts
+++ client/CLI/src/myFtpClient.ts
@@ -1,111 +1,113 @@
-import { createConnection, createServer, AddressInfo, Socket } from 'net'
-import { readFileSync, createWriteStream } from 'fs';
+import { createWriteStream, readFileSync } from "fs"
+import * as net from "net"
 
-if (process.argv.length < 4) throw "You must provide host and port in arguments !"
+if (process.argv.length < 4) { throw new Error("You must provide host and port in arguments !") }
 
 const HOST = process.argv[2]
-const PORT = parseInt(process.argv[3])
-let isSkipRes = 0;
+const PORT = parseInt(process.argv[3], 10)
+let isSkipRes = 0
 
 const openDataServer = (
-  onServerUp: (onDone: () => void) => void,
-  onClientConnect: (dataClient: Socket, onDone: () => void) => void,
-  onClientData: (dataClient: Socket, data: String, onDone: () => void) => void,
-  onClientEnd: () => void,
+  onServerUp?: () => void,
+  onClientConnect?: (dataClient: net.Socket, onDone: () => void) => void,
+  onClientData?: (dataClient: net.Socket, data: string, onDone: () => void) => void,
+  onClientEnd?: () => void,
 ) => {
   const done = () => {
     server.close()
   }
 
-  let server = createServer(dataClient => {
-    onClientConnect(dataClient, () => {
-      dataClient.end()
-      done()
-    })
-
-    dataClient.on('data', (data: Buffer) => {
-      console.log(data.toString());
-      
-      onClientData(dataClient, data.toString(), () => {
+  const server = net.createServer((dataClient: net.Socket) => {
+    if (onClientConnect) {
+      onClientConnect(dataClient, () => {
         dataClient.end()
         done()
       })
+    }
+
+    dataClient.on("data", (data: Buffer) => {
+      console.log(data.toString())
+
+      if (onClientData) {
+        onClientData(dataClient, data.toString(), () => {
+          dataClient.end()
+          done()
+        })
+      }
     })
 
-    dataClient.on('end', () => {
-      onClientEnd()
+    dataClient.on("end", () => {
+      if (onClientEnd) { onClientEnd() }
       done()
     })
   })
 
   server.listen(() => {
-    let address = server.address() as AddressInfo
+    const address = server.address() as net.AddressInfo
     isSkipRes = 2
-    client.write(`EPRT |${address.family === 'IPv6' ? 2 : 1}|${address.address}|${address.port}\r\n`)
-    client.once('data', _ => { onServerUp(done) })
+    client.write(`EPRT |${address.family === "IPv6" ? 2 : 1}|${address.address}|${address.port}\r\n`)
+    client.once("data", (_) => {
+      if (onServerUp) { onServerUp() }
+    })
   })
 }
 
-const client = createConnection(PORT, HOST, () => {
-  console.log(`Connected to FTP Server "${HOST}:${PORT}"`);
+const client = net.createConnection(PORT, HOST, () => {
+  console.log(`Connected to FTP Server "${HOST}:${PORT}"`)
 })
 
-client.on('error', (err: Error) => { throw err })
-client.on('end', (err: Error) => { throw "Server disconnected :/" })
+client.on("error", (err: Error) => { throw err })
+client.on("end", (err: Error) => { throw new Error("Server disconnected :/") })
 
-client.on('data', (data: Buffer) => {
+client.on("data", (data: Buffer) => {
   const response = data.toString().trim()
   console.log(`Response: ${response}`)
 
-  const respCode = parseInt(response.substring(0, 3))
+  const respCode = parseInt(response.substring(0, 3), 10)
 
-  if (isSkipRes > 0) isSkipRes--
-  if (isSkipRes == 0 && respCode != 150) {
+  if (isSkipRes > 0) { isSkipRes-- }
+  if (isSkipRes === 0 && respCode !== 150) {
     process.stdout.write("\nCommand: ")
   }
 })
 
-process.openStdin().addListener("data", (data) => {
-  const input: string = `${data}`.trim()
+process.openStdin().addListener("data", (stdin) => {
+  const input = `${stdin}`.trim()
 
-  if (input === 'LIST') {
+  if (input === "LIST") {
     openDataServer(
-      (onDone) => { client.write("LIST\r\n") },
-      (onDone) => { },
-      (dataClient, data, onDone) => {
+      () => { client.write("LIST\r\n") },
+      null,
+      (_, __, onDone) => {
         onDone()
       },
-      () => { }
     )
-  } else if (input.startsWith('STOR ')) {
-    const fileName = input.split(' ')[1];
+  } else if (input.startsWith("STOR ")) {
+    const fileName = input.split(" ")[1]
 
-    let fileContent = readFileSync(fileName).toString()
+    const fileContent = readFileSync(fileName).toString()
 
     openDataServer(
-      (onDone) => { client.write(data) },
+      () => { client.write(input + "\r\n") },
       (dataClient, onDone) => {
-        console.log(`Sending ${fileName} to server...`);
+        console.log(`Sending ${fileName} to server...`)
 
         dataClient.write(fileContent, onDone)
         dataClient.end()
       },
-      (dataClient, data, onDone) => { },
-
-      () => { }
     )
-  } else if (input.startsWith('RETR ')) {
-    const fileName = input.split(' ')[1];
+  } else if (input.startsWith("RETR ")) {
+    const fileName = input.split(" ")[1]
 
-    let writeStream = createWriteStream(fileName)
+    const writeStream = createWriteStream(fileName)
 
     openDataServer(
-      (onDone) => { client.write(data) },
-      (dataClient, onDone) => { },
-      (dataClient, data, onDone) => { writeStream.write(data) },
+      () => { client.write(input + "\r\n") },
+      null,
+      (_, data, __) => { writeStream.write(data) },
       () => { writeStream.end() }
     )
   } else {
-    client.write(data)
+    client.write(input + "\r\n")
   }
 })
